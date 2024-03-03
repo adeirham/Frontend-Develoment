a666ba88bf3263c4962459bf3ab7d6975323a389
adeirham <deirham00@gmail.com>
2024-03-03 16:34:07 +0700

    belajar react

---
+++ kuliah/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../Componen/Header";
+import Utama from "../Componen/Utama";
+
+function Home() {
+    return ( 
+        <>
+            <Header/>
+            <Utama/>
+        </>
+     );
+}
+
+export default Home;
